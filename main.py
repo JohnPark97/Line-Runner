fbe2b54e732d06a9568a5794b885c9070afcab35
John Park <h.johnpark97@gmail.com>
2023-08-20 22:49:45 -0700

    add static images

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import pygame
 import os
 from player import Player
 
-# Window configs
-WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 24
-
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -14,12 +9,22 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
 # Images
-MIDDLE_TILE = pygame.image.load(os.path.join('Assets', 'Tiles', 'middle_tile.png'))
-UP_TILE = pygame.image.load(os.path.join('Assets', 'Tiles', 'up_tile.png'))
-DOWN_TILE = pygame.image.load(os.path.join('Assets', 'Tiles', 'down_tile.png'))
+TILE_WIDTH = 64
+TILE_HEIGHT = 64
+
+MIDDLE_TILE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'Tiles', 'middle_tile.png')), (TILE_WIDTH, TILE_HEIGHT))
+UP_TILE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'Tiles', 'up_tile.png')), (TILE_WIDTH, TILE_HEIGHT))
+DOWN_TILE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'Tiles', 'down_tile.png')), (TILE_WIDTH, TILE_HEIGHT))
 
 # Player
+TILE_X_OFFSET = 32
+TILE_Y_OFFSET = TILE_HEIGHT/2 + 16
 RUNNING_PLAYER = pygame.sprite.Group()
+
+# Window configs
+WIDTH, HEIGHT = TILE_WIDTH * 12, TILE_HEIGHT * 8
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 4
 
 # Set the caption of the window
 pygame.display.set_caption("Line Runner")
@@ -29,8 +34,13 @@ def draw_window():
     WIN.fill(WHITE)
 
     # map tiles
+    i = 0
+    while i < WIDTH:
+        WIN.blit(MIDDLE_TILE, (i, HEIGHT/2))
+        i += TILE_WIDTH
     WIN.blit(MIDDLE_TILE, (WIDTH/2, HEIGHT/2))
-    WIN.blit(MIDDLE_TILE, (WIDTH/2, HEIGHT/2))
+    # WIN.blit(UP_TILE, (WIDTH/2 + TILE_WIDTH, HEIGHT/2))
+    # WIN.blit(DOWN_TILE, (WIDTH/2 + TILE_WIDTH * 2, HEIGHT/2))
 
     # player
     RUNNING_PLAYER.draw(WIN)
@@ -43,8 +53,8 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    player = Player(100, 100)
-    RUNNING_PLAYER.add(player)
+    player = Player(TILE_X_OFFSET, HEIGHT/2 - TILE_Y_OFFSET)
+    # RUNNING_PLAYER.add(player)
 
     while run:
         clock.tick(FPS)
--- player.py
+++ player.py
@@ -1,19 +1,25 @@
 import pygame
+from enum import Enum, auto
+
+class PlayerState(Enum) :
+    IDLE : auto()
+    RUNNING : auto()
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x_pos, y_pos):
         super().__init__()
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.sprites = []
-        self.sprites.append(pygame.image.load('Assets/running_1.png'))
-        self.sprites.append(pygame.image.load('Assets/running_2.png'))
+        self.state: PlayerState = PlayerState.RUNNING
 
-        self.current_sprite = 0
-        self.image = self.sprites[self.current_sprite]
-
+        # character position
         self.rect = self.image.get_rect()
         self.rect.topleft = [x_pos, y_pos]
+
+        # character animation
+        self.animation = self.load_animation()
+
 
     def x_pos(self):
         return self.x_pos
@@ -21,11 +27,30 @@ class Player(pygame.sprite.Sprite):
     def y_pos(self):
         return self.y_pos
 
+    def set_state(self, state: PlayerState):
+        if self.state != state:
+            self.state = state
 
-    def update(self):
+    def load_animation(self):
+        if self.state == PlayerState.RUNNING:
+            self.load_running_animation()
+        elif self.state == PlayerState.IDLE:
+            self.load_idle_animation()
+
         self.current_sprite += 1
 
         if self.current_sprite >= len(self.sprites):
             self.current_sprite = 0
         
         self.image = self.sprites[self.current_sprite]
+
+    def load_running_animation(self):
+        self.sprites = []
+        self.sprites.append(pygame.image.load('Assets/running_1.png'))
+        self.sprites.append(pygame.image.load('Assets/running_2.png'))
+
+    def load_idle_animation(self):
+        self.sprites = []
+        self.sprites.append(pygame.image.load('Assets/running_1.png'))
+        self.sprites.append(pygame.image.load('Assets/running_2.png'))
+
