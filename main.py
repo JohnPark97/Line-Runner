40d7086a40a8b83511f343cc8aa94ef9fd95507e
John Park <h.johnpark97@gmail.com>
2023-08-21 21:44:20 -0700

    make a map scrollable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
 import pygame
 import os
+import enum
 from player import Player
 
 # Colors
@@ -11,6 +13,8 @@ YELLOW = (255, 255, 0)
 # Images
 TILE_WIDTH = 64
 TILE_HEIGHT = 64
+TILE_SPEED = 5
+scroll_x = 0
 
 MIDDLE_TILE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'Tiles', 'middle_tile.png')), (TILE_WIDTH, TILE_HEIGHT))
 UP_TILE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'Tiles', 'up_tile.png')), (TILE_WIDTH, TILE_HEIGHT))
@@ -24,23 +28,38 @@ RUNNING_PLAYER = pygame.sprite.Group()
 # Window configs
 WIDTH, HEIGHT = TILE_WIDTH * 12, TILE_HEIGHT * 8
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 4
+FPS = 12
 
 # Set the caption of the window
 pygame.display.set_caption("Line Runner")
 
-def draw_window():
-    # background
+def load_map(level: int = 1):
     WIN.fill(WHITE)
 
-    # map tiles
+    with open(os.path.join('Assets', 'Maps', f'{level}.json')) as file:
+        data = json.load(file)
+
+    tiles = data['tiles']
+
     i = 0
-    while i < WIDTH:
-        WIN.blit(MIDDLE_TILE, (i, HEIGHT/2))
+    for tile in tiles:
+        y = HEIGHT/2
+        if tile == 0:
+            WIN.blit(MIDDLE_TILE, (i - scroll_x, y))
+        elif tile == 1:
+            WIN.blit(UP_TILE, (i - scroll_x, y))
+        elif tile == 2:
+            WIN.blit(DOWN_TILE, (i - scroll_x, y))
         i += TILE_WIDTH
-    WIN.blit(MIDDLE_TILE, (WIDTH/2, HEIGHT/2))
-    # WIN.blit(UP_TILE, (WIDTH/2 + TILE_WIDTH, HEIGHT/2))
-    # WIN.blit(DOWN_TILE, (WIDTH/2 + TILE_WIDTH * 2, HEIGHT/2))
+
+def draw_window(is_map_loaded: bool):
+    if is_map_loaded == False:
+        load_map()
+        is_map_loaded = True  
+    
+    # scroll the map to the left
+    global scroll_x
+    scroll_x += TILE_SPEED
 
     # player
     RUNNING_PLAYER.draw(WIN)
@@ -55,15 +74,19 @@ def main():
 
     player = Player(TILE_X_OFFSET, HEIGHT/2 - TILE_Y_OFFSET)
     RUNNING_PLAYER.add(player)
+    frame = 0
 
+    is_map_loaded = False
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
+        frame += 1
+        draw_window(is_map_loaded)
+
         player.load_animation()
-        draw_window()
 
     pygame.quit()
 
